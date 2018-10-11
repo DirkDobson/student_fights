965ec203271815af014973a5e1195d96b89679b5
Dirk Dobson <dirkvancouver@live.com>
2018-10-11 10:11:45 -0600

    render students

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 var loaded = false;
+var students = [];
+
+function renderStudents() {
+  var list = document.getElementById('students')
+  list.innerHTML = null;
+  students.forEach( function(student) {
+    var li = document.getElementById('li')
+    li.innerText = student.name
+    list.append(li)
+  })
+}
+
+function sample() {
+  var index = Math.floor(Math.random() * students.length)
+  var student = students[index]
+  students.splice(index, 1)
+  return student
+}
+
+function selectStudents() {
+  var left = sample();
+  var right = sample();
+  renderStudents();
+  return [left, right]
+}
+
+function startFight() {
+  loaded = true
+  var left = document.getElementById('left')
+  var right = document.getElementById('right')
+  left.className = 'left fight-box';
+  right.className = 'left fight-box';
+  var fighting = selectStudents();
+  console.log(fighting)
+}
 function pullStudents() {
   if (!loaded) {
     var xhr = new XMLHttpRequest()
     xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-        var students = JSON.parse(xhr.responseText);
-        var list = document.getElementById('students');
-        students.forEach( function(student) {
-          var li = document.createElement('li')
-          li.innerText = student.name
-          list.append(li);
-        })
-        loaded = true 
-        document.getElementById('load_students').remove()
+        students = JSON.parse(xhr.responseText);
+        renderStudents();
+        document.getElementById('load_students').remove();
+        startFight();
       }
     }
     xhr.open('GET', 'https://canvas-students.herokuapp.com/api/student_list/64', true)
